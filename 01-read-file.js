7e93eb145d73f8e4dc857610e3383384305d0956
Serhii Didusenko <didusenko40@gmail.com>
2023-05-10 00:37:05 +0300

    init: add two tasks

--- 01-read-file.js
+++ 01-read-file.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
-const read = fs.readFile('text.txt', 'utf8', (error, content) => {
+fs.readFile('./01-read-file/text.txt', 'utf8', (error, content) => {
   if (error) throw error;
   console.log(content);
 });
-
--- 02-write-file.js
+++ 02-write-file.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const { stdin, stdout } = process;
 
-fs.writeFile('text.txt', '', error => {
+fs.writeFile('./02-write-file/text.txt', '', error => {
   if (error) throw error;
 })
 
 stdout.write('Введите текст:\n');
 stdin.on('data', data => {
-  fs.appendFile('text.txt', data, error => {
+  fs.appendFile('./02-write-file/text.txt', data, error => {
     if (error) throw error;
   })
 }
